168432372bf0d5b6b4fcdd9a64f8018703d7b4f7
yangning <ning@edison.tech>
2022-05-09 16:30:50 +0800

    ON-976: lose className for div

--- src/web/editor/formats/block/index.ts
+++ src/web/editor/formats/block/index.ts
@@ -1,8 +1,43 @@
 import { Quill } from "react-quill";
 
 const Default = Quill.import("blots/block");
+const ATTRIBUTES = ["id", "class"];
 
-class Block extends Default {}
+class Block extends Default {
+  static create(value: any) {
+    let node = super.create(value);
+    if (value) {
+      ATTRIBUTES.forEach((attr) => {
+        if (value[attr]) {
+          node.setAttribute(attr, value[attr]);
+        }
+      });
+    }
+    return node;
+  }
+
+  static formats(domNode: Element) {
+    const attr: { [key: string]: any } = {};
+    return ATTRIBUTES.reduce((formats, attribute) => {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, attr);
+  }
+
+  format(name: string, value: string | number) {
+    if (ATTRIBUTES.includes(name)) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
 Block.blotName = "block";
 Block.tagName = "div";
 
